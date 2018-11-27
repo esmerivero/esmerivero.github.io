b3ece585d3491e41ab99c5cc0724d7fee96e9bac
lameme <esme.riveroh@gmail.com>
2018-11-27 15:56:36 -0600

    Actualizando cv y acerca de mi

--- src/components/AcercaDeMi.js
+++ src/components/AcercaDeMi.js
@@ -10,11 +10,16 @@ export default class AcercaDeMi extends Component {
                     <div id="image" className="cropcircle"></div>
                 </section>
                 <section className="col-xs-12 col-sm-12 col-md-6 col-lg-6 info">
-                    <h2 className="tracking"><b>Acerca de mi</b></h2>
-                    <h4>¡Hola! Soy Esmeralda Rivero, coder Front-End</h4>
-                    <p>
-                        Disfruto mucho de las buenas conexiones de internet, videojuegos,
-                        música, conciertos y de la programación.
+                    <h2 className="tracking"><b>Acerca de mí</b></h2>
+                    <h4>¡Hola! Soy Esmeralda Rivero, coder Front-End.</h4>
+                    <br/>
+                    <p className="text-about-me">
+                        Autodidacta por la tecnología desde los 10 años, formada como
+                        Ing. Sistemas Computacionales.
+                        <br/>
+                        Disfruto compartir mis conocimientos ya que a medida que explico algún tema,
+                        surgen nuevos puntos de vistas a mis soluciones y me hacen mejorar.
+                        <br/>
                         Mis herramientas favoritas tecnológicas (hasta ahora) son React y Bootstrap.
                     </p>
                     <Curriculum />
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ export default class Header extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div><img src={logo} className="logo-rivero" alt="Logo del portafolio" /></div>
+        
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -15,7 +16,7 @@ export default class Header extends Component {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#acerca-de-mi">Acerca de mi <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="#acerca-de-mi">Acerca de mí <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#proyectos">Proyectos</a>
