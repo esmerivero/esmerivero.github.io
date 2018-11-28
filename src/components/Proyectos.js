b367ab6d4d276c27294de84ef9935eb89f10eb5f
lameme <esme.riveroh@gmail.com>
2018-11-27 23:23:56 -0600

    Corrigiendo cards de proyectos

--- src/components/Proyectos.js
+++ src/components/Proyectos.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import BurgerQueen from '../assets/burger-queen.JPG';
-import Datadashboard from '../assets/data-dashboard.JPG';
-import MovieApi from '../assets/movie-api.JPG';
+import RegistroDeVisitantes from '../assets/registro-de-visitantes.JPG';
+import Mdlinks from '../assets/mdlinks.JPG';
 import './Proyectos.css'
 
 export default class Proyectos extends Component{
@@ -18,13 +18,14 @@ export default class Proyectos extends Component{
                     <div className="card">
                         <img className="card-img-top" src={BurgerQueen} alt="Pantalla principal del proyecto burger queen"/>
                         <h5 className="card-title">Burger Queen</h5>
-                        <p className="card-text">Progressive Web App, para toma de pedidos de establecimiento de comida rápida.
-                        Con funcionalidad offline, visualización de historial de pedidos.
-                        Desarrollado con React, Firebase, Bootstrap</p>
+                        <p className="card-text">Web App, para toma de pedidos de establecimiento de comida rápida.
+                        Con funcionalidad offline y visualización de historial de pedidos.
+                        <br/>
+                        Desarrollado en React, Node, Express, Firebase, Bootstrap, desplegado en Herokuapp</p>
                         <div className="row">
-                        <a href="https://esmerivero.github.io/burger-queen" target="_blank" rel="noreferrer noopener"
+                        <a href="https://young-savannah-58223.herokuapp.com/" target="_blank" rel="noreferrer noopener"
                         className="btn btn-secondary col">Demo</a>
-                        <a href="https://github.com/esmerivero/burger-queen" target="_blank" rel="noreferrer noopener"
+                        <a href="https://github.com/esmerivero/burgerqueen-node" target="_blank" rel="noreferrer noopener"
                         className="btn btn-secondary col">Código</a>
                         </div>
                     </div>
@@ -32,15 +33,19 @@ export default class Proyectos extends Component{
 
                     <article className="col-lg-4 col-md-4 col-sm-8 col-xs-8">
                     <div className="card">
-                        <img className="card-img-top" src={Datadashboard} alt="Pantalla principal del proyecto datadashboard"/>
-                        <h5 className="card-title">Datadashboard</h5>
-                        <p className="card-text">Visualizador de data, para las Training Manager de Laboratoria.
-                        Filtrando la información del JSON por país, sede, generación.
-                        Esta v2.0 fue creada con React y Materialize.</p>
+                        <img className="card-img-top" src={RegistroDeVisitantes} alt="Pantalla principal del proyecto datadashboard"/>
+                        <h5 className="card-title">Registro de visitantes</h5>
+                        <p className="card-text">Aplicación web que agiliza el proceso de registro de visitantes a coworking.
+                        <br/>
+                        La vista 1 es para los visitantes y la vista 2 (user: admin@admin.com pass: admin) está diseñada para los administradores del coworking.
+                        Autenticación y almacenamiento de datos en Firebase.
+                        </p>
                         <div className="row">
-                        <a href="https://esmerivero.github.io/burger-queen" target="_blank" rel="noreferrer noopener"
-                        className="btn btn-secondary col ">Demo</a>
-                        <a href="https://github.com/esmerivero/burger-queen" target="_blank" rel="noreferrer noopener"
+                        <a href="https://adrianaesvereidytv.github.io/Proyecto-5--Easy-Visit/src/views/registro.html" target="_blank" rel="noreferrer noopener"
+                        className="btn btn-secondary col ">Vista 1</a>
+                        <a href="https://adrianaesvereidytv.github.io/Proyecto-5--Easy-Visit/src/views/login.html" target="_blank" rel="noreferrer noopener"
+                        className="btn btn-secondary col ">Vista 2</a>
+                        <a href="https://github.com/AdrianaEsvereidyTV/Proyecto-5--Easy-Visit" target="_blank" rel="noreferrer noopener"
                         className="btn btn-secondary col">Código</a>
                         </div>
                     </div>
@@ -48,14 +53,15 @@ export default class Proyectos extends Component{
 
                     <article className="col-lg-4 col-md-4 col-sm-8 col-xs-8">
                     <div className="card">
-                        <img className="card-img-top" src={MovieApi} alt="Pantalla principal del prpyecto Movie Api"/>
-                        <h5 className="card-title">3, 2, 1 ¡Acción!</h5>
-                        <p className="card-text">Buscador de peliculas, que arroja sus datos como título, fecha de estreno y genero
-                        Este proyecto consume API's, creado en vanilla con Bootstrap.</p>
+                        <img className="card-img-top" src={Mdlinks} alt="Pantalla principal del prpyecto Movie Api"/>
+                        <h5 className="card-title">Markdown Links</h5>
+                        <p className="card-text">Acercamiento con NodeJS para realizar una busqueda con los detalles de links
+                        en archivos con extensión .md .
+                        Dentro de un array se despliegan los objetos con cada detalle del link, como la liga, el status,
+                        su descripción.
+                        </p>
                         <div className="row">
-                        <a href="https://esmerivero.github.io/cdmx20181-Track-FE-themovieapi/src/views/billboard.html" 
-                        target="_blank" rel="noreferrer noopener" className="btn btn-secondary col">Demo</a>
-                        <a href="https://github.com/esmerivero/cdmx20181-Track-FE-themovieapi" 
+                        <a href="https://github.com/esmerivero/cdmx-2018-01-FE-markdown" 
                         target="_blank" rel="noreferrer noopener" className="btn btn-secondary col">Código</a>
                         </div>
                     </div>
